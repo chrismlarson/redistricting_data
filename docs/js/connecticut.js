23c11a402496c0c0fc784ee6af0db05be7f81032
Chris Larson <larson.chris@gmail.com>
2020-01-11 19:23:59 -0500

    Adjusting center of CT map

--- docs/js/connecticut.js
+++ docs/js/connecticut.js
@@ -1,4 +1,4 @@
-var mymap = L.map('mapid').setView([41.628951,-72.5942876], 9);
+var mymap = L.map('mapid').setView([41.5776064,-72.591541], 9);
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
